18d07588265fadb006515bc7bbdc5e6ef1fc1f98
Bram Borggreve <borggreve@gmail.com>
2017-11-30 17:08:11 +0800

    UI Component: navigation

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,11 +3,13 @@ import { NgModule } from '@angular/core';
 
 
 import { AppComponent } from './app.component';
+import { NavComponent } from './nav/nav.component';
 
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    NavComponent
   ],
   imports: [
     BrowserModule
